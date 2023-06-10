282ee362f309f2967dd06cc9d79eb5e529464fc4
Trelel129 <ferrynur77@gmail.com>
2023-06-10 15:06:09 +0700

    multithread rooms

--- semut orang gajah/server.py
+++ semut orang gajah/server.py
@@ -1,6 +1,7 @@
 # Server side
 import socket
 import random
+import threading
 
 # Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,6 +21,7 @@ s.listen(5)
 # Define moves
 moves = ["semut", "orang", "gajah"]
 
+
 # Define outcomes
 outcomes = {
     ("semut", "semut"): "draw",
@@ -33,95 +35,50 @@ outcomes = {
     ("gajah", "gajah"): "draw"
 }
 
-# Create a list to store the connected clients and their moves
-clients = []
-
 # Create a dictionary to store the room codes and the clients in each room
 rooms = {}
 
-# Accept two connections from the clients
-for i in range(2):
-    # Accept a connection from a client
-    c, addr = s.accept()
-    print("Got connection from", addr)
+# Define a function to handle the game logic for each room
+def game_room(code):
+    # Get the list of clients in that room
+    clients = rooms[code]
 
-    # Send a welcome message to the client
-    c.send(("Welcome to gajah semut orang! You are player " + str(i+1)).encode())
 
-    # Ask the client to enter a room code or create a new one
-    c.send(b"Please enter a room code or type 'new' to create a new room")
+    # Start the game loop
 
-    # Receive the client's input
-    code = c.recv(1024).decode()
-
-    # Check if the client wants to create a new room
-    if code == "new":
-        # Generate a random four-digit room code
-        code = str(random.randint(1000, 9999))
-
-        # Send the room code to the client
-        c.send(("Your room code is: " + code).encode())
-
-        # Create a new entry in the rooms dictionary with the code as the key and an empty list as the value
-        rooms[code] = []
-
-        # Append the client and its move to the list of clients in that room
-        rooms[code].append([c, None])
-
-    else:
-        # Check if the room code exists in the rooms dictionary
-        if code in rooms:
-            # Append the client and its move to the list of clients in that room
-            rooms[code].append([c, None])
-            # Send a message to the client that they have joined the room
-            c.send(("You have joined room: " + code).encode()) 
-        else:
-            # Send an error message to the client that the room code is invalid
-            c.send(b"Invalid room code. Please try again")
-
-            # Close the connection
-            c.close()
-
-            # Break out of the loop
-            break
-
-# Start the game loop
-while True:
-    # Loop through each room in the rooms dictionary
-    for code in rooms:
-        # Get the list of clients in that room
-        clients = rooms[code]
-        if len(clients) == 2:
+    while True:
         # Loop through each client in that room
-            for i in range(2):
-                # Get the client and its move from the list
-                c, move = clients[i]
+        for i in range(2):
+            # Get the client and its move from the list
+            c, move = clients[i]
 
-                # Try to receive the client's move
-                try:
-                    move = c.recv(1024).decode()
-                except:
-                    # If an error occurs, assume the client has disconnected
-                    print("Player", i+1, "has disconnected")
-                    move = "quit"
+            # Try to receive the client's move
+            try:
+                move = c.recv(1024).decode()
+            except:
+                # If an error occurs, assume the client has disconnected
+                print("Player", i+1, "has disconnected")
+                move = "quit"
 
-                # Check if the client wants to quit
-                if move == "quit":
-                    # Send a goodbye message to the client
-                    c.send(b"Goodbye!")
+            # Check if the client wants to quit
+            if move == "quit":
+                # Send a goodbye message to the client
+                c.send(b"Goodbye!")
 
-                    # Close the connection
-                    c.close()
+                # Close the connection
+                c.close()
 
-                    # Remove the client from the list of clients in that room
-                    clients.pop(i)
+                # Remove the client from the list of clients in that room
+                clients.pop(i)
 
-                    # Break out of the loop
-                    break
+                # Break out of the loop
+                break
 
-                # Update the client's move in the list of clients in that room
-                clients[i][1] = move
+            # Update the client's move in the list of clients in that room
+            clients[i][1] = move
 
+        # Check if there are still two clients in that room
+        if len(clients) == 2:
             # Get the moves of both clients in that room
             move1 = clients[0][1]
             move2 = clients[1][1]
@@ -161,7 +118,58 @@ while True:
             # Break out of the loop
             break
 
-    # Check if there are any rooms left in the rooms dictionary
-    if not rooms:
-        # Break out of the loop
-        break
+# Accept connections from the clients indefinitely
+while True: 
+
+    # Accept a connection from a client 
+    c, addr = s.accept()
+    print("Got connection from", addr)
+
+    # Send a welcome message to the client
+    c.send(b"Welcome to gajah semut orang!")
+
+    # Ask the client to enter a room code or create a new one
+    c.send(b"Please enter a room code or type 'new' to create a new room")
+
+    # Receive the client's input
+    code = c.recv(1024).decode()
+
+    # Check if the client wants to create a new room
+    if code == "new":
+        # Generate a random four-digit room code
+        code = str(random.randint(1000, 9999))
+
+        # Send the room code to the client
+        c.send(("Your room code is: " + code).encode())
+
+        # Create a new entry in the rooms dictionary with the code as the key and an empty list as the value
+        rooms[code] = []
+
+        # Append the client and its move to the list of clients in that room
+        rooms[code].append([c, None])
+
+        # Create a new thread for that room and start it
+        t = threading.Thread(target=game_room, args=(code,))
+        t.start()
+
+    else:
+        # Check if the room code exists in the rooms dictionary
+        if code in rooms:
+            # Append the client and its move to the list of clients in that room
+            rooms[code].append([c, None])
+
+            # Send a message to the client that they have joined the room
+            c.send(("You have joined room: " + code).encode())
+
+            # Check if there are two clients in that room
+            if len(rooms[code]) == 2:
+                # Create a new thread for that room and start it
+                t = threading.Thread(target=game_room, args=(code,))
+                t.start()
+
+        else:
+            # Send an error message to the client that the room code is invalid
+            c.send(b"Invalid room code. Please try again")
+
+            # Close the connection
+            c.close()
