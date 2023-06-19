551c1cad48ccf7a8f68b510a510ed0cc13fa18a5
zzungokong <fantasania@gmail.com>
2023-06-19 09:37:36 +0700

    swith hand

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -8,53 +8,44 @@ def update_countdown(count):
         root.after(1000, update_countdown, count - 1) 
     else :
         countdown.config(text="Time's Up!") 
-        hitam_button.config(state="disabled")
-        putih_button.config(state="disabled")
 
-def switch_to_putih1():
-    user2.config(image=putih1)  
-
-def switch_to_hitam1():
-    user2.config(image=hitam1)
+def switch_to_hitam2():
+    user2.config(image=hitam2)
 
 def switch_to_putih2():
     user2.config(image=putih2)  
-    tulisan2.config(text="Putih")
-
-def switch_to_hitam2():
-    user2.config(image=hitam2) 
-    tulisan2.config(text="Hitam") 
 
 #basenya
 root = Tk()
 root.title("Hompimpa")
 root.configure(background="#9b59b6")
 
-putih1 = ImageTk.PhotoImage(Image.open("aset/Putih.png").resize((300, 300)).rotate(180))
 hitam1 = ImageTk.PhotoImage(Image.open("aset/Hitam.png").resize((300, 300)).rotate(180))
+putih1 = ImageTk.PhotoImage(Image.open("aset/Putih.png").resize((300, 300)).rotate(180))
+hitam2 = ImageTk.PhotoImage(Image.open("aset/Putih.png").resize((300, 300)))
 putih2 = ImageTk.PhotoImage(Image.open("aset/Putih.png").resize((300, 300)))
-hitam2 = ImageTk.PhotoImage(Image.open("aset/Hitam.png").resize((300, 300)))
 
 user1 = Label(root, image=hitam1, background="#9b59b6")
 user2 = Label(root, image=putih2, background="#9b59b6")
+user1.grid(row=1, column=5)
+user2.grid(row=10, column=5)
 
 tulisan1 = Label(root, text="Hitam", font=100, bg="#9b59b6", fg="white" )
 tulisan2 = Label(root, text="Putih", font=100, bg="#9b59b6", fg="white" )
-opponent = Label(root, text="OPPONENT", font=100, bg="#9b59b6", fg="white" )
+tulisan1.grid(row=2, column=5)
+tulisan2.grid(row=9, column=5)
 
 countdown = Label(root, text="", font= 500, bg="#9b59b6", fg="black")
+countdown.grid(row = 3, column=5)
+
 
 hitam_button = Button(root, text="Hitam", command=switch_to_hitam2)
-putih_button = Button(root, text="Putih", command=switch_to_putih2)
-
-opponent.grid(row=1, column = 5)
-user1.grid(row=2, column=5)
-tulisan1.grid(row=3, column=5)
-countdown.grid(row = 4, column=5)
-tulisan2.grid(row=9, column=5)
-user2.grid(row=10, column=5)
 hitam_button.grid(row=11, column=4)
+
+putih_button = Button(root, text="Putih", command=switch_to_putih2)
 putih_button.grid(row=11, column=6)
 
-update_countdown(10)
+
+
+update_countdown(5)
 root.mainloop()
