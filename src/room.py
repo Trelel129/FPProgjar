e75fa656c6a9c8fa785dd5ced4987970f16db07e
adifa <user@users-MacBook-Pro.local>
2023-07-05 14:29:23 +0700

    room

--- src/room.py
+++ src/room.py
@@ -21,18 +21,3 @@ class room(tk.Frame):
     def create_widgets(root):
         back_button = ttk.Button(root, text="Back to Menu", command=root.menu_manager.show_main_menu)
         back_button.pack()
-
-        # create_button = ttk.Button(root.background_canvas, image=root.create_room_btn_photo, command=root.show_create_menu)
-        # create_button.place(x=1100, y=450)
-
-        # join_button = ttk.Button(root.background_canvas, image=root.join_room_btn_photo, command=root.show_join_menu)
-        # join_button.place(x=1100, y=600)
-
-    # def show_create_menu(root):
-    #     root.menu_manager.show_menu("create")
-
-    # def show_join_menu(root):
-    #     root.menu_manager.show_menu("join")
-
-    # def show_how_to_play_menu(root):
-    #     root.menu_manager.show_menu("how_to_play")
